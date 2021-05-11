cc15a750c2d5d22c973630aa0ada32644132dfe5
Felipe Salles <prof.felipe.salles@gmail.com>
2021-05-11 00:06:58 -0300

    first commit

---
+++ api-cadastro-cep/src/main/java/com/felipesalles/apicadastrocep/ApiCadastroCepApplication.java
@@ -0,0 +1,13 @@
+package com.felipesalles.apicadastrocep;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiCadastroCepApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiCadastroCepApplication.class, args);
+	}
+
+}
---
+++ api-cadastro-cep/src/test/java/com/felipesalles/apicadastrocep/ApiCadastroCepApplicationTests.java
@@ -0,0 +1,13 @@
+package com.felipesalles.apicadastrocep;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiCadastroCepApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
